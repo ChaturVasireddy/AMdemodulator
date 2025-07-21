ab4c7e6eb1f3fec35a1608aed1fadab6f6d190dd
ChaturVasireddy <chaturvasireddy@gmail.com>
2025-07-21 23:08:43 +0530

    added gitignore for cache files

--- mcu_rt_testing/ti_am_demodulator/main.c
+++ mcu_rt_testing/ti_am_demodulator/main.c
@@ -1,3 +1,4 @@
+
 //
 // Included Files
 //
@@ -7,7 +8,6 @@
 //
 // Defines
 //
-#define RESULTS_BUFFER_SIZE     32
 #define EX_ADC_RESOLUTION       12
 // 12 for 12-bit conversion resolution, which supports (ADC_MODE_SINGLE_ENDED)
 // Sample on single pin (VREFLO is the low reference)
@@ -18,17 +18,16 @@
 //
 // Globals
 //
-uint16_t adcAResults[RESULTS_BUFFER_SIZE];   // Buffer for results
-uint16_t index;                              // Index into result buffer
-volatile uint16_t bufferFull;                // Flag to indicate buffer is full
+uint16_t adcAResult0;
+uint16_t adcAResult1;
+uint16_t adcDResult0;
+uint16_t adcDResult1;
 
 //
 // Function Prototypes
 //
-void initADC(void);
-void initEPWM(void);
-void initADCSOC(void);
-__interrupt void adcA1ISR(void);
+void initADCs(void);
+void initADCSOCs(void);
 
 //
 // Main
@@ -57,33 +56,10 @@ void main(void)
     Interrupt_initVectorTable();
 
     //
-    // Interrupts that are used in this example are re-mapped to ISR functions
-    // found within this file.
+    // Set up ADCs, initializing the SOCs to be triggered by software
     //
-    Interrupt_register(INT_ADCA1, &adcA1ISR);
-
-    //
-    // Set up the ADC and the ePWM and initialize the SOC
-    //
-    initADC();
-    initEPWM();
-    initADCSOC();
-
-    //
-    // Initialize results buffer
-    //
-    for(index = 0; index < RESULTS_BUFFER_SIZE; index++)
-    {
-        adcAResults[index] = 0;
-    }
-
-    index = 0;
-    bufferFull = 0;
-
-    //
-    // Enable ADC interrupt
-    //
-    Interrupt_enable(INT_ADCA1);
+    initADCs();
+    initADCSOCs();
 
     //
     // Enable Global Interrupt (INTM) and realtime interrupt (DBGM)
@@ -97,29 +73,38 @@ void main(void)
     while(1)
     {
         //
-        // Start ePWM1, enabling SOCA and putting the counter in up-count mode
+        // Convert, wait for completion, and store results
         //
-        EPWM_enableADCTrigger(EPWM1_BASE, EPWM_SOC_A);
-        EPWM_setTimeBaseCounterMode(EPWM1_BASE, EPWM_COUNTER_MODE_UP);
+        ADC_forceMultipleSOC(ADCA_BASE, (ADC_FORCE_SOC0 | ADC_FORCE_SOC1));
 
         //
-        // Wait while ePWM1 causes ADC conversions which then cause interrupts.
-        // When the results buffer is filled, the bufferFull flag will be set.
+        // Wait for ADCA to complete, then acknowledge flag
         //
-        while(bufferFull == 0)
+        while(ADC_getInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1) == false)
         {
         }
-        bufferFull = 0;     // Clear the buffer full flag
+        ADC_clearInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1);
+
+        ADC_forceMultipleSOC(ADCD_BASE, (ADC_FORCE_SOC0 | ADC_FORCE_SOC1));
+        //
+        // Wait for ADCD to complete, then acknowledge flag
+        //
+        while(ADC_getInterruptStatus(ADCD_BASE, ADC_INT_NUMBER1) == false)
+        {
+        }
+        ADC_clearInterruptStatus(ADCD_BASE, ADC_INT_NUMBER1);
 
         //
-        // Stop ePWM1, disabling SOCA and freezing the counter
+        // Store results
         //
-        EPWM_disableADCTrigger(EPWM1_BASE, EPWM_SOC_A);
-        EPWM_setTimeBaseCounterMode(EPWM1_BASE, EPWM_COUNTER_MODE_STOP_FREEZE);
+        adcAResult0 = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER0);
+        adcAResult1 = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER1);
+        adcDResult0 = ADC_readResult(ADCDRESULT_BASE, ADC_SOC_NUMBER0);
+        adcDResult1 = ADC_readResult(ADCDRESULT_BASE, ADC_SOC_NUMBER1);
 
         //
         // Software breakpoint. At this point, conversion results are stored in
-        // adcAResults.
+        // adcAResult0, adcAResult1, adcDResult0, and adcDResult1.
         //
         // Hit run again to get updated conversions.
         //
@@ -128,15 +113,17 @@ void main(void)
 }
 
 //
-// Function to configure and power up ADCA.
+// Function to configure and power up ADCs A and C.
 //
-void initADC(void)
+void initADCs(void)
 {
-    
+
+
     //
     // Set ADCDLK divider to /4
     //
     ADC_setPrescaler(ADCA_BASE, ADC_CLK_DIV_4_0);
+    ADC_setPrescaler(ADCD_BASE, ADC_CLK_DIV_4_0);
 
     //
     // Set resolution and signal mode (see #defines above) and load
@@ -144,69 +131,37 @@ void initADC(void)
     //
 #if(EX_ADC_RESOLUTION == 12)
     ADC_setMode(ADCA_BASE, ADC_RESOLUTION_12BIT, ADC_MODE_SINGLE_ENDED);
+    ADC_setMode(ADCD_BASE, ADC_RESOLUTION_12BIT, ADC_MODE_SINGLE_ENDED);
 #elif(EX_ADC_RESOLUTION == 16)
     ADC_setMode(ADCA_BASE, ADC_RESOLUTION_16BIT, ADC_MODE_DIFFERENTIAL);
+    ADC_setMode(ADCD_BASE, ADC_RESOLUTION_16BIT, ADC_MODE_DIFFERENTIAL);
 #endif
 
     //
     // Set pulse positions to late
     //
     ADC_setInterruptPulseMode(ADCA_BASE, ADC_PULSE_END_OF_CONV);
+    ADC_setInterruptPulseMode(ADCD_BASE, ADC_PULSE_END_OF_CONV);
 
     //
-    // Power up the ADC and then delay for 1 ms
+    // Power up the ADCs and then delay for 1 ms
     //
     ADC_enableConverter(ADCA_BASE);
+    ADC_enableConverter(ADCD_BASE);
+
     DEVICE_DELAY_US(1000);
 }
 
 //
-// Function to configure ePWM1 to generate the SOC.
+// Function to configure SOCs 0 and 1 of ADCs A and C.
 //
-void initEPWM(void)
+void initADCSOCs(void)
 {
     //
-    // Disable SOCA
-    //
-    EPWM_disableADCTrigger(EPWM1_BASE, EPWM_SOC_A);
-
-    //
-    // Configure the SOC to occur on the first up-count event
-    //
-    EPWM_setADCTriggerSource(EPWM1_BASE, EPWM_SOC_A, EPWM_SOC_TBCTR_U_CMPA);
-    EPWM_setADCTriggerEventPrescale(EPWM1_BASE, EPWM_SOC_A, 1);
-
-    //
-    // Set the compare A value to 1000 and the period to 1999
-    // Assuming ePWM clock is 100MHz, this would give 50kHz sampling
-    // 50MHz ePWM clock would give 25kHz sampling, etc. 
-    // The sample rate can also be modulated by changing the ePWM period
-    // directly (ensure that the compare A value is less than the period). 
-    //
-    EPWM_setCounterCompareValue(EPWM1_BASE, EPWM_COUNTER_COMPARE_A, 1000);
-    EPWM_setTimeBasePeriod(EPWM1_BASE, 1999);
-
-    //
-    // Set the local ePWM module clock divider to /1
-    //
-    EPWM_setClockPrescaler(EPWM1_BASE,
-                           EPWM_CLOCK_DIVIDER_1,
-                           EPWM_HSCLOCK_DIVIDER_1);
-
-    //
-    // Freeze the counter
-    //
-    EPWM_setTimeBaseCounterMode(EPWM1_BASE, EPWM_COUNTER_MODE_STOP_FREEZE);
-}
-
-//
-// Function to configure ADCA's SOC0 to be triggered by ePWM1.
-//
-void initADCSOC(void)
-{
-    //
-    // Configure SOC0 of ADCA to convert pin A0. The EPWM1SOCA signal will be
-    // the trigger.
+    // Configure SOCs of ADCA
+    // - SOC0 will convert pin A0.
+    // - SOC1 will convert pin A1.
+    // - Both will be triggered by software only.
     // - For 12-bit resolution, a sampling window of 15 (75 ns at a 200MHz
     //   SYSCLK rate) will be used.  For 16-bit resolution, a sampling window
     //   of 64 (320 ns at a 200MHz SYSCLK rate) will be used.
@@ -217,57 +172,56 @@ void initADCSOC(void)
     //
 
 #if(EX_ADC_RESOLUTION == 12)
-    ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_EPWM1_SOCA,
+    ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_SW_ONLY,
                  ADC_CH_ADCIN0, 15);
+    ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER1, ADC_TRIGGER_SW_ONLY,
+                 ADC_CH_ADCIN1, 15);
 #elif(EX_ADC_RESOLUTION == 16)
-    ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_EPWM1_SOCA,
+    ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_SW_ONLY,
                  ADC_CH_ADCIN0, 64);
+    ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER1, ADC_TRIGGER_SW_ONLY,
+                 ADC_CH_ADCIN1, 64);
 #endif
 
     //
-    // Set SOC0 to set the interrupt 1 flag. Enable the interrupt and make
+    // Set SOC1 to set the interrupt 1 flag. Enable the interrupt and make
     // sure its flag is cleared.
     //
-    ADC_setInterruptSource(ADCA_BASE, ADC_INT_NUMBER1, ADC_SOC_NUMBER0);
+    ADC_setInterruptSource(ADCA_BASE, ADC_INT_NUMBER1, ADC_SOC_NUMBER1);
     ADC_enableInterrupt(ADCA_BASE, ADC_INT_NUMBER1);
     ADC_clearInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1);
-}
-
-//
-// ADC A Interrupt 1 ISR
-//
-__interrupt void adcA1ISR(void)
-{
-    //
-    // Add the latest result to the buffer
-    //
-    adcAResults[index++] = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER0);
 
     //
-    // Set the bufferFull flag if the buffer is full
+    // Configure SOCs of ADCD
+    // - SOC0 will convert pin D2.
+    // - SOC1 will convert pin D3.
+    // - Both will be triggered by software only.
+    // - For 12-bit resolution, a sampling window of 15 (75 ns at a 200MHz
+    //   SYSCLK rate) will be used.  For 16-bit resolution, a sampling window
+    //   of 64 (320 ns at a 200MHz SYSCLK rate) will be used.
+    // - NOTE: A longer sampling window will be required if the ADC driving
+    //   source is less than ideal (an ideal source would be a high bandwidth
+    //   op-amp with a small series resistance). See TI application report
+    //   SPRACT6 for guidance on ADC driver design.
     //
-    if(RESULTS_BUFFER_SIZE <= index)
-    {
-        index = 0;
-        bufferFull = 1;
-    }
+
+#if(EX_ADC_RESOLUTION == 12)
+    ADC_setupSOC(ADCD_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_SW_ONLY,
+                 ADC_CH_ADCIN2, 15);
+    ADC_setupSOC(ADCD_BASE, ADC_SOC_NUMBER1, ADC_TRIGGER_SW_ONLY,
+                 ADC_CH_ADCIN3, 15);
+#elif(EX_ADC_RESOLUTION == 16)
+    ADC_setupSOC(ADCD_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_SW_ONLY,
+                 ADC_CH_ADCIN2, 64);
+    ADC_setupSOC(ADCD_BASE, ADC_SOC_NUMBER1, ADC_TRIGGER_SW_ONLY,
+                 ADC_CH_ADCIN3, 64);
+#endif
 
     //
-    // Clear the interrupt flag
+    // Set SOC1 to set the interrupt 1 flag. Enable the interrupt and make
+    // sure its flag is cleared.
     //
-    ADC_clearInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1);
-
-    //
-    // Check if overflow has occurred
-    //
-    if(true == ADC_getInterruptOverflowStatus(ADCA_BASE, ADC_INT_NUMBER1))
-    {
-        ADC_clearInterruptOverflowStatus(ADCA_BASE, ADC_INT_NUMBER1);
-        ADC_clearInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1);
-    }
-
-    //
-    // Acknowledge the interrupt
-    //
-    Interrupt_clearACKGroup(INTERRUPT_ACK_GROUP1);
+    ADC_setInterruptSource(ADCD_BASE, ADC_INT_NUMBER1, ADC_SOC_NUMBER1);
+    ADC_enableInterrupt(ADCD_BASE, ADC_INT_NUMBER1);
+    ADC_clearInterruptStatus(ADCD_BASE, ADC_INT_NUMBER1);
 }
